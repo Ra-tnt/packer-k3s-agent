b095c6dfb49782b80623cfafcdc704736fbdd883
Bryan Dollery <bryan.dollery@gmail.com>
2020-07-22 00:26:50 +0300

    worked out how to provision the agent's token

--- provision.sh
+++ provision.sh
@@ -3,11 +3,26 @@ sudo apt update -y && sudo apt install -y curl vim jq git make
 curl -Ls get.docker.com | sh
 sudo usermod -aG docker ubuntu
 export K3S_HOST=controlplane.bryan.dobc
+export K3S_TOKEN=wibble
 export K3S_URL=http://$K3S_HOST:6443
 curl -sfL https://get.k3s.io | sh -
 
+sudo touch /etc/systemd/system/configK3s.service
+sudo chmod a+rw /etc/systemd/system/configK3s.service
+sudo chmod a+rw /etc/systemd/system/k3s-agent.service.env
 
+cat << EOF > /etc/systemd/system/configK3s.service
+[Unit]
+Description=Configures the environment for k3s
+After=k3s.target
 
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+ExecStart=printf "K3S_TOKEN=%s\nK3S_URL=%s" "$(curl -Ss http://controlplane.bryan.dobc:8765/node)" "$K3S_URL" >> /etc/systemd/system/k3s-agent.service.env
 
-export K3S_TOKEN=$(curl http://controlplane.bryan.dobc:8765/node)
-echo $K3S_TOKEN
+[Install]
+WantedBy=multi-user.target
+EOF
+
