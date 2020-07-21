d709de0ce9cd706365e38590bb1b324378b0562e
Bryan Dollery <bryan.dollery@gmail.com>
2020-07-21 23:52:21 +0300

    added tags, updated readme, and added owner

--- provision.sh
+++ provision.sh
@@ -4,6 +4,10 @@ curl -Ls get.docker.com | sh
 sudo usermod -aG docker ubuntu
 export K3S_HOST=controlplane.bryan.dobc
 export K3S_URL=http://$K3S_HOST:6443
-export K3S_TOKEN=$(curl http://$K3S_HOST:8765/node)
-echo $K3S_TOKEN
 curl -sfL https://get.k3s.io | sh -
+
+
+
+
+export K3S_TOKEN=$(curl http://controlplane.bryan.dobc:8765/node)
+echo $K3S_TOKEN
