96f8d529f478d3e1d4b08af2703e4caea9aecd0e
Bryan Dollery <bryan.dollery@gmail.com>
2020-07-23 14:54:24 +0300

    updated to new url

--- provision.sh
+++ provision.sh
@@ -9,20 +9,23 @@ curl -sfL https://get.k3s.io | sh -
 
 sudo touch /etc/systemd/system/configK3s.service
 sudo chmod a+rw /etc/systemd/system/configK3s.service
+sudo touch /etc/systemd/system/k3s-agent.service.env
 sudo chmod a+rw /etc/systemd/system/k3s-agent.service.env
 
 cat << 'EOF' > /etc/systemd/system/configK3s.service
 [Unit]
 Description=Configures the environment for k3s
-After=k3s.target
+Before=k3s.target
 
 [Service]
 Type=simple
 Restart=always
 RestartSec=1
-ExecStart=printf "K3S_TOKEN=%s\nK3S_URL=%s" "$$(curl -Ss http://controlplane.bryan.dobc:8765/node)" "$K3S_URL" >> /etc/systemd/system/k3s-agent.service.env
+ExecStart=printf "K3S_TOKEN=%s\nK3S_URL=%s" "$$(curl -Ss http://controlplane.bryan.dobc:8765)" "https://controlplane.bryan.dobc:6443" > /etc/systemd/system/k3s-agent.service.env
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+sudo systemctl enable configK3s
+sudo systemctl start configK3s
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# A packer configuration to create a K3S agent on an ubuntu base. It reads the node key from the server at http://controlplane.bryan.dobc:8765/node
+\# A packer configuration to create a K3S agent on an ubuntu base. It reads the node key from the server at http://controlplane.bryan.dobc:8765
 
 This requires access to the server on ports 8765 and 6443
 
